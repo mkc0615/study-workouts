d3bc2528f89a31687cc5c30c70ceefddea8b4154
mkc0615 <mkc0615@gmail.com>
2021-11-06 16:54:53 +0900

    JPA implemented
    Recap on JPA use in spring boot
    Switched all use of jdbc in project to JPA/Hibernate

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,30 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
     private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource){
+    public SpringConfig(DataSource dataSource, EntityManager em){
         this.dataSource = dataSource;
+        this.em = em;
     }
+
+//  @Autowired
+//  public SpringConfig(DataSource dataSource){
+//      this.dataSource = dataSource;
+//  }
 
     @Bean
     public MemberService memberService(){
@@ -32,6 +37,7 @@ public class SpringConfig {
           // 순수 jdbc 사용시
 //        return new JdbcMemberRepository();
 //        return new MemoryMemberRepository();
-          return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
