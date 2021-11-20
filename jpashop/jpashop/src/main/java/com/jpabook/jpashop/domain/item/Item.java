6df2e30134f533ad208370473f1528a574eef63f
mkc0615 <mkc0615@gmail.com>
2021-11-20 15:34:49 +0900

    Item related functions added
    - Item domain methods added
    - Item repository added
    - Item service added

--- jpashop/jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ jpashop/jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpabook.jpashop.domain.item;
 
 import com.jpabook.jpashop.domain.Category;
+import com.jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 
 import javax.persistence.*;
@@ -23,5 +24,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //=business logic=//
+    // 재고 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+    // 재고 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("Need More Stock!");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
