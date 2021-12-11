cd5114ae951327922ca065e296888b348839acbf
Min Kee Choi <minkeechoi@Minui-MacBookPro.local>
2021-12-11 21:45:49 +0900

    Added api controller for saving new members

---
+++ jpashop/jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,52 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    // api 작성시에는 entity를 param으로 쓰면 안된다
+    // entity 외부 노출은 후에 많은 에러의 요인이 된다
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    // v2가 better practice
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    public class CreateMemberRequest{
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
