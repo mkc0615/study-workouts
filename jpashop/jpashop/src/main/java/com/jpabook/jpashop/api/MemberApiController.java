d3ef7dacdd70ca4558b5642d3fffee9a8c68e4f3
Min Kee Choi <mkc0615@gmail.com>
2021-12-16 22:04:23 +0900

    Added member modification and inspection

--- jpashop/jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -2,14 +2,15 @@ package com.jpabook.jpashop.api;
 
 import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -17,6 +18,34 @@ import javax.validation.constraints.NotEmpty;
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    // 안 좋은 예시 -> entity의 직접 노출로 인해 안 좋음
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+    // 외부에 노출할 데이터만 dto로 감싸서 반환
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m->new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
 
     // api 작성시에는 entity를 param으로 쓰면 안된다
     // entity 외부 노출은 후에 많은 에러의 요인이 된다
@@ -35,7 +64,21 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    // 회원수정 API
+    @PutMapping("/api/v2/member/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request){
+
+        memberService.update(id, request.getName());
+
+        // return 값은 차라리 쿼리를 하나 만들어서 쿼리와 커멘드를 분리하는걸 추천!
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
     // entity 이용이 위험하므로 dto를 사용
+    // 실무에서는 entity가 자주 바뀌기 때문에 들어오고 나가는 api에서 장애가 자주 발생한다
     @Data
     public class CreateMemberRequest{
         @NotEmpty
@@ -49,5 +92,17 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
     }
 }
