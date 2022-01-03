aead0a3e4f68bc80052460bb0107c9d4759e177f
Min Kee Choi <mkc0615@gmail.com>
2022-01-03 21:50:06 +0900

    Added order api using collections

---
+++ jpashop/jpashop/src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,83 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.Repository.OrderRepository;
+import com.jpabook.jpashop.Repository.OrderSearch;
+import com.jpabook.jpashop.domain.Address;
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    // Not good : entity exposed!
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> resultList = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : resultList) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o->o.getItem().getName());
+        }
+        return resultList;
+    }
+
+    // better than v1 , but to many queries
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+
+    }
+
+    @Data
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+           orderItems = order.getOrderItems().stream()
+                   .map(orderItem -> new OrderItemDto(orderItem))
+                   .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem){
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getItem().getPrice();
+            count = orderItem.getCount();
+        }
+    }
+}
