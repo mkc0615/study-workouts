4fd9173e994eaa06267c7197c71e0b5ea199211f
Min Kee Choi <mkc0615@gmail.com>
2022-04-28 23:34:24 +0900

    EDIT QueryBasicTest with search options

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -13,6 +13,10 @@ import study.querydsl.domain.Team;
 
 import javax.persistence.EntityManager;
 
+import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.domain.QMember.member;
+
+
 @SpringBootTest
 @Transactional
 public class QuerydslBasicTest {
@@ -20,8 +24,11 @@ public class QuerydslBasicTest {
     @Autowired
     EntityManager em;
 
+    JPAQueryFactory queryFactory;
+
     @BeforeEach
     public void before(){
+        queryFactory = new JPAQueryFactory(em);
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -44,23 +51,65 @@ public class QuerydslBasicTest {
                 .setParameter("username", "member1")
                 .getSingleResult();
 
-        Assertions.assertThat(findSingleMember.getUsername()).isEqualTo("member1");
+        assertThat(findSingleMember.getUsername()).isEqualTo("member1");
     }
 
     @Test
     public void startQuerydsl(){
-        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-
-        QMember m = new QMember("m");
+        // removed below by import static
+        // QMember m = new QMember("m");
 
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
-        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
-
+        assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
+    @Test
+    public void search(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    // search options
+    public void otherSearches(){
+
+        member.username.eq("member1"); // =
+        member.username.ne("member1"); // !=
+        member.username.eq("member1").not(); // !=
+
+        member.username.isNotNull(); // username is not null
+
+        member.age.in(10, 20); // age in (10, 20)
+        member.age.notIn(10, 20); // age not in (10, 20)
+        member.age.between(10, 20); // age between 10 , 20
+
+        member.age.goe(30); // >=
+        member.age.gt(30); // >
+        member.age.loe(30); // <=
+        member.age.lt(30); // <
+
+        member.username.like("%member%"); // like search
+        member.username.contains("member"); // like %member% search
+        member.username.startsWith("member"); // like member% search
+    }
 }
